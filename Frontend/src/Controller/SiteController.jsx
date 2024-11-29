4a0c02d3e4b90651ae99cf136c2b0b49474dc5ab
champa lal jangid <champalaljangid9292@gmail.com>
2024-11-29 07:45:38 +0530

    fixing

--- Frontend/src/Controller/SiteController.jsx
+++ Frontend/src/Controller/SiteController.jsx
@@ -1,7 +1,8 @@
 // This is the controller file where one change reflects to everywhere.
 
 const SITE_CONFIG = {
-  apiIPMongo: "http://localhost:5001",
+  apiIPMongo: "https://vrv-eta.vercel.app/",
+  // apiIPMongo: "http://localhost:5001",
   apiToken: "a1b2c3d4",
   apiImage: "http://localhost:5001/images/",
 };
